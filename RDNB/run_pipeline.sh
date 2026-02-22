a2b7ab7c47e83632cfcff33ee8b282406dcd4051
Carlos Banjar <carlosesb@dcc.ufrj.br>
2026-02-22 15:47:22 -0300

    Fix input file paths in grid_search.sub and update comments in run_pipeline.sh

--- RDNB/run_pipeline.sh
+++ RDNB/run_pipeline.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Descompacta o ambiente virtual enviado pelo Condor [cite: 409, 410]
+# Descompacta o ambiente e o dataset no nó escravo
 tar -xzf projeto_rdn.tar.gz
+tar -xzf dataset.tar.gz
 
-# Executa o Python usando o interpretador do nó, mas com as libs do venv injetadas
+# Executa o experimento
 python3 run_rdn_cv.py --max_depth $1 --node_size $2 --n_estimators $3 --data_path ./dataset --output_path ./result_$1_$2_$3
