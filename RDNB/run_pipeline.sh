11f2f44d16b839f75457549af2574a91c2db9c6a
Carlos Banjar <carlosesb@dcc.ufrj.br>
2026-02-22 15:39:26 -0300

    Update comments and improve clarity in run_pipeline.sh

--- RDNB/run_pipeline.sh
+++ RDNB/run_pipeline.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# O Condor passa os argumentos $1 (depth), $2 (node), $3 (trees)
+# Descompacta o ambiente virtual enviado pelo Condor [cite: 409, 410]
+tar -xzf projeto_rdn.tar.gz
+
+# Executa o Python usando o interpretador do nó, mas com as libs do venv injetadas
 python3 run_rdn_cv.py --max_depth $1 --node_size $2 --n_estimators $3 --data_path ./dataset --output_path ./result_$1_$2_$3
