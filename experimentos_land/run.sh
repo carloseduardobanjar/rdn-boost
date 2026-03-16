aea3ba10b063781c3d968049c7f294435b3ca6dc
Carlos Banjar <carlosesb@dcc.ufrj.br>
2026-03-15 22:54:10 -0300

    Fix virtual environment path in run.sh for consistency

--- experimentos_land/run.sh
+++ experimentos_land/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Ativa o venv usando o caminho completo
-source /home/cschuller/rdn-boost/venv_land/bin/activate
+source /home/users/cschuller/rdn-boost/venv_land/bin/activate
 
 # Executa o Python usando o caminho completo para o script
 # E repassa todos os argumentos com "$@"
-python3 /home/cschuller/rdn-boost/experimentos_land/run_rdn_cv_simplified.py "$@"
+python3 /home/users/cschuller/rdn-boost/experimentos_land/run_rdn_cv_simplified.py "$@"
