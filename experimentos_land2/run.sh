2f965a141c73d57311e901a91d03264d222f5c21
Carlos Banjar <carlosesb@dcc.ufrj.br>
2026-03-19 13:42:07 -0300

    Remove output directory creation and permissions from run.sh

--- experimentos_land2/run.sh
+++ experimentos_land2/run.sh
@@ -6,10 +6,6 @@ echo "Args: $@"
 
 cd /home/users/cschuller/rdn-boost/experimentos_land2
 
-rm -rf output
-mkdir output
-chmod 777 output
-
 source /home/users/cschuller/venv_land/bin/activate
 
 python3 -u /home/users/cschuller/rdn-boost/experimentos_land2/run_rdn_cv_simplified.py "$@"
