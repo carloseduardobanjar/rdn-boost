ab982edf7feb21af62f5a876f82685431844fb2e
Carlos Banjar <carlosesb@dcc.ufrj.br>
2026-03-21 15:46:47 -0300

    Remove BSRL_DATA_DIR export and directory creation from run.sh

--- experimentos_land2/run.sh
+++ experimentos_land2/run.sh
@@ -8,9 +8,6 @@ cd /home/users/cschuller/rdn-boost/experimentos_land2
 
 source /home/users/cschuller/ambiente_land/bin/activate
 
-export BSRL_DATA_DIR=/home/users/cschuller/tmp/bsrl_data
-mkdir -p $BSRL_DATA_DIR
-
 python3 -u /home/users/cschuller/rdn-boost/experimentos_land2/run_rdn_cv_simplified.py "$@"
 
 echo "==== FIM ===="
